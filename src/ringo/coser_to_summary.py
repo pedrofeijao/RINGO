10b6af9d0b96e0d87542dd07a5ecd778c19d1f08
Pedro Feijao <pedrofeijao1975@gmail.com>
2016-09-03 08:16:04 +0200

    small change

--- src/ringo/coser_to_summary.py
+++ src/ringo/coser_to_summary.py
@@ -38,5 +38,5 @@ if __name__ == '__main__':
     print "\t".join(["File", "Dist", "Dup1", "Dup2", "RunningTime", "Gap"])
     for sol_file in param.files:
         r = parse_coser_sol(sol_file)
-        title = sol_file.replace(".out", "").replace("coser.", "").replace("G_","").replace(".","_")
+        title = sol_file.replace(".out", "").replace("coser.", "").replace(".","_")
         print "\t".join([title] + [str(x) for x in r])
