eadcd3499c4030a3a16a6ddd72a41c50406b2486
Pedro Feijao <pedrofeijao1975@gmail.com>
2016-09-05 14:19:32 +0200

    script with LD simulation of Shao, 2015 for COSER styls sims

---
+++ src/ringo/LD_simulation.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python2
+import ringo_config
+
+cfg = ringo_config.RingoConfig()
+import pyximport;pyximport.install(build_dir=cfg.pyximport_build())
+import argparse
+import os
+import random
+import math
+from dendropy import Tree
+from dendropy.simulate import treesim
+import numpy as np
+import algorithms
+import file_ops
+import model
+from simulation import Simulation, SimParameters, EventType, RearrangementType
+
+
+def run_L_D_simulation(self, L, D):
+    # L = duplication length
+    # D = number of DCJs in each branch.
+    #
+    param = self.sim_parameters
+    # pre_dups (at root) and post_dups (at branches) to achieve 1.5 genes/family in average.
+    pre_duplications = int(0.2 * param.num_genes / L)
+    post_duplications = int(0.3 * param.num_genes / L)
+    print "dups:", pre_duplications, post_duplications
+    current_copy_number = None  # will init at root
+    deletion_length_range = xrange(1, param.indel_length + 1)
+    duplication_length_range = xrange(1, L + 1)
+    idx = 1
+    ev_tree = self.sim_tree
+    for ev_node in ev_tree.preorder_node_iter():
+        if ev_node.parent_node is None:
+            # identity genome:
+            ev_node.value = current_genome = model.Genome.identity(param.num_genes, param.num_chr)
+
+            # add copy number information to track orthologous/paralogous, when duplications are present:
+            for chromosome in current_genome.chromosomes:
+                chromosome.copy_number = [1] * len(chromosome.gene_order)
+            current_copy_number = current_genome.gene_count()
+            # pre-duplications:
+            for i in range(pre_duplications):
+                Simulation.apply_random_segmental_duplication(current_genome,
+                                                              range(1, param.duplication_length + 1),
+                                                              current_copy_number)
+
+            ev_node.rearrangements = ev_node.insertions = ev_node.deletions = ev_node.duplications = 0
+            if ev_node.label is None:
+                ev_node.label = "Root"
+        else:
+            # evolve genome:
+            if ev_node.is_internal():
+                if ev_node.label is None:
+                    ev_node.label = "M%02d" % idx
+                    idx += 1
+            else:  # complete labelling for leaves
+                ev_node.label = ev_node.taxon.label
+
+            current_genome = ev_node.parent_node.value.clone(ev_node.label)
+            ev_node.value = current_genome
+            ev_node.edge.length = D
+            # events
+            events = [EventType.DUPLICATION] * post_duplications + [EventType.REARRANGEMENT] * D
+            random.shuffle(events)
+            for event in events:
+                if event == EventType.DUPLICATION:
+                    Simulation.apply_random_segmental_duplication(current_genome, duplication_length_range, current_copy_number)
+                elif event == EventType.REARRANGEMENT:
+                    # here, I can also have deletions:
+                    ev = np.random.choice([RearrangementType.REVERSAL, EventType.DELETION], 1,
+                                          p=[param.rearrangement_p, param.deletion_p])
+                    if ev == RearrangementType.REVERSAL:
+                        Simulation.apply_random_reversal(current_genome)
+                    else:
+                        Simulation.apply_random_deletion(current_genome, deletion_length_range)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Simulates rearrangement evolution on a given newick tree")
+    parser.add_argument("-s", "--sim", type=int, help="Simulate a new birth_death with SIM species")
+    parser.add_argument("-n", "--num_genes", type=int, default=100, help="Number of genes in the root genome.")
+    parser.add_argument("-c", "--num_chr", type=int, default=5, help="Number of chromosomes in the root genome.")
+    parser.add_argument("-L", "-dl", "--duplication_length", type=int, default=5, help="Maximum length of duplication event.")
+    parser.add_argument("-D", "--rearrangements", type=int, default=5, help="Number of rearrangements.")
+
+    parser.add_argument("-o", "--output", type=str, default="sim", help="Name of the output folder.")
+    parser.add_argument("-dp", "--deletion_p", type=float, default=0.0, help="Percentage of deletions, from 0 to 1.0")
+    parser.add_argument("-ip", "--insertion_p", type=float, default=0.0, help="Percentage of insertions, from 0 to 1.0")
+    parser.add_argument("-il", "--indel_length", type=int, default=5, help="Maximum size of indel event in genes.")
+
+    parser.add_argument("-d", "--disturb", type=float, default=0,
+                        help="Disturb branch lengths multiplying each by e^r, where r in [-d,+d]. ")
+    param = parser.parse_args()
+
+    # Simulation parameters:
+    sim_par = SimParameters(num_genes=param.num_genes, num_chr=param.num_chr,
+                            del_p=param.deletion_p, ins_p=param.insertion_p, indel_length=param.indel_length,
+                            duplication_length=param.duplication_length)
+    # start sim object;
+    sim = Simulation(param.output, sim_par)
+
+    sim.simulate_tree(param.sim)
+
+    run_L_D_simulation(sim, param.duplication_length, param.rearrangements)
+
+    sim.save_simulation(save_copies=True)
--- src/ringo/simulation.py
+++ src/ringo/simulation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python2
 import ringo_config
+
 cfg = ringo_config.RingoConfig()
-import pyximport; pyximport.install(build_dir=cfg.pyximport_build())
+import pyximport;pyximport.install(build_dir=cfg.pyximport_build())
 import argparse
 import os
 import random
@@ -12,15 +13,16 @@ import numpy as np
 import algorithms
 import file_ops
 import model
-from ringo_config import RingoConfig
 
 __author__ = 'pfeijao'
+
 
 # Config names:
 
 # noinspection PyClassHasNoInit
 class EventType:
     REARRANGEMENT, DELETION, INSERTION, DUPLICATION = range(4)
+
 
 # noinspection PyClassHasNoInit
 class RearrangementType:
@@ -29,7 +31,7 @@ class RearrangementType:
 
 class SimParameters:
     def __init__(self, num_genes=0, num_chr=0, del_p=0, ins_p=0, indel_length=0, duplication_p=0,
-                 duplication_length=0, rate=0, scale=0, disturb=0):
+                 duplication_length=0, rate=0, scale=0, disturb=0, pre_dup=0):
         self.num_genes = num_genes
         self.num_chr = num_chr
         self.deletion_p = del_p
@@ -40,6 +42,8 @@ class SimParameters:
         self.rate = rate
         self.scale = scale
         self.disturb = disturb
+        self.pre_duplications = pre_dup
+
         # Rearrangement, Insertion and Deletion prob:
         self.rearrangement_p = 1 - del_p - ins_p - duplication_p
 
@@ -53,6 +57,7 @@ class SimParameters:
             self.reversal_p = 1
             self.transposition_p = 0
             self.translocation_p = 0
+
 
 class Simulation:
     def __init__(self, folder, sim_parameters=None):
@@ -79,7 +84,7 @@ class Simulation:
     @staticmethod
     def apply_random_reversal(genome):
         chromosome = np.random.choice(genome.chromosomes)
-        bp = sorted(np.random.choice(len(chromosome.gene_order)+1, 2))
+        bp = sorted(np.random.choice(len(chromosome.gene_order) + 1, 2))
         chromosome.gene_order[bp[0]:bp[1]] = reversed([-x for x in chromosome.gene_order[bp[0]:bp[1]]])
         if chromosome.copy_number is not None:
             chromosome.copy_number[bp[0]:bp[1]] = reversed(chromosome.copy_number[bp[0]:bp[1]])
@@ -87,10 +92,11 @@ class Simulation:
     @staticmethod
     def apply_random_transposition(genome):
         chromosome = np.random.choice(genome.chromosomes)
-        bp = sorted(np.random.choice(len(chromosome.gene_order)+1, 3))
+        bp = sorted(np.random.choice(len(chromosome.gene_order) + 1, 3))
         chromosome.gene_order[bp[0]:bp[2]] = chromosome.gene_order[bp[1]:bp[2]] + chromosome.gene_order[bp[0]:bp[1]]
         if chromosome.copy_number is not None:
-            chromosome.copy_number[bp[0]:bp[2]] = chromosome.copy_number[bp[1]:bp[2]] + chromosome.copy_number[bp[0]:bp[1]]
+            chromosome.copy_number[bp[0]:bp[2]] = chromosome.copy_number[bp[1]:bp[2]] + chromosome.copy_number[
+                                                                                        bp[0]:bp[1]]
 
     @staticmethod
     def apply_random_translocation(genome):
@@ -122,7 +128,7 @@ class Simulation:
         if bp + length > chromosome.length():
             length = chromosome.length() - bp
         # position:
-        position = np.random.choice(range(bp) + range(bp+length, chromosome.length()))
+        position = np.random.choice(range(bp) + range(bp + length, chromosome.length()))
         block = chromosome.gene_order[bp:bp + length]
         # update gene copy number
         for gene in block:
@@ -156,6 +162,19 @@ class Simulation:
             chromosome.copy_number[bp:bp] = [1] * length
         return gene + length
 
+    @staticmethod
+    def apply_random_rearrangement(param, genome):
+        rearrangement = np.random.choice([RearrangementType.REVERSAL, RearrangementType.TRANSLOCATION,
+                                          RearrangementType.TRANSPOSITION], 1,
+                                         p=[param.reversal_p, param.translocation_p, param.transposition_p])
+        if rearrangement == RearrangementType.REVERSAL:
+            Simulation.apply_random_reversal(genome)
+        elif rearrangement == RearrangementType.TRANSLOCATION:
+            Simulation.apply_random_translocation(genome)
+        elif rearrangement == RearrangementType.TRANSPOSITION:
+            Simulation.apply_random_transposition(genome)
+        else:
+            raise RuntimeError("Unknown rearrangement type.")
 
     @staticmethod
     def apply_random_events(param, genome, n, current_insertion_gene, current_copy_number=None):
@@ -165,34 +184,26 @@ class Simulation:
         duplication_count = 0
         # param = self.sim_parameters
 
-        insertion_length_range = xrange(1, param.indel_length+1)
-        deletion_length_range = xrange(1, param.indel_length+1)
-        duplication_length_range = xrange(1, param.duplication_length+1)
+        insertion_length_range = xrange(1, param.indel_length + 1)
+        deletion_length_range = xrange(1, param.indel_length + 1)
+        duplication_length_range = xrange(1, param.duplication_length + 1)
 
         # choose events and apply:
-        events = np.random.choice([EventType.REARRANGEMENT, EventType.INSERTION, EventType.DELETION, EventType.DUPLICATION], n,
-                                  p=[param.rearrangement_p, param.insertion_p, param.deletion_p, param.duplication_p])
+        events = np.random.choice(
+            [EventType.REARRANGEMENT, EventType.INSERTION, EventType.DELETION, EventType.DUPLICATION], n,
+            p=[param.rearrangement_p, param.insertion_p, param.deletion_p, param.duplication_p])
 
         for event in events:  # number of events, can be weighted by 'scaling' parameters
             if event == EventType.REARRANGEMENT:
-                rearrangement = np.random.choice([RearrangementType.REVERSAL, RearrangementType.TRANSLOCATION,
-                                                  RearrangementType.TRANSPOSITION], 1,
-                                                 p=[param.reversal_p, param.translocation_p, param.transposition_p])
-                if rearrangement == RearrangementType.REVERSAL:
-                    Simulation.apply_random_reversal(genome)
-                elif rearrangement == RearrangementType.TRANSLOCATION:
-                    Simulation.apply_random_translocation(genome)
-                elif rearrangement == RearrangementType.TRANSPOSITION:
-                    Simulation.apply_random_transposition(genome)
-                else:
-                    raise RuntimeError("Unknown rearrangement type.")
+                Simulation.apply_random_rearrangement(param, genome)
                 rearrangement_count += 1
 
             elif event == EventType.DELETION:
                 Simulation.apply_random_deletion(genome, deletion_length_range)
                 deletion_count += 1
             elif event == EventType.INSERTION:
-                current_insertion_gene = Simulation.apply_random_insertion(genome, current_insertion_gene, insertion_length_range, current_copy_number)
+                current_insertion_gene = Simulation.apply_random_insertion(genome, current_insertion_gene,
+                                                                           insertion_length_range, current_copy_number)
                 insertion_count += 1
             elif event == EventType.DUPLICATION:
                 # Simulation.apply_random_tandem_duplication(genome, duplication_length_range)
@@ -203,7 +214,6 @@ class Simulation:
                 raise RuntimeError("Unknown evolutionary event.")
         return rearrangement_count, insertion_count, deletion_count, duplication_count, current_insertion_gene
 
-
     def run_simulation(self):
 
         param = self.sim_parameters
@@ -211,14 +221,25 @@ class Simulation:
 
         # current insertion genes: (new genes)
         current_insertion_gene = param.num_genes + 1
-
+        current_copy_number = None  # will init at root
         idx = 1
         ev_tree = self.sim_tree
         for ev_node in ev_tree.preorder_node_iter():
             if ev_node.parent_node is None:
                 # identity genome:
-                ev_node.value = model.Genome.identity(param.num_genes, param.num_chr)
-                ev_node.rearrangements = ev_node.insertions = ev_node.deletions = 0
+                ev_node.value = current_genome = model.Genome.identity(param.num_genes, param.num_chr)
+
+                # add copy number information to track orthologous/paralogous, when duplications are present:
+                for chromosome in current_genome.chromosomes:
+                    chromosome.copy_number = [1] * len(chromosome.gene_order)
+                current_copy_number = current_genome.gene_count()
+                # pre-duplications:
+                for i in range(param.pre_duplications):
+                    Simulation.apply_random_segmental_duplication(current_genome,
+                                                                  range(1, param.duplication_length + 1),
+                                                                  current_copy_number)
+
+                ev_node.rearrangements = ev_node.insertions = ev_node.deletions = ev_node.duplications = 0
                 if ev_node.label is None:
                     ev_node.label = "Root"
             else:
@@ -236,43 +257,48 @@ class Simulation:
 
                 # evolution
                 rearrangements, insertions, deletions, duplications, current_insertion_gene = \
-                    Simulation.apply_random_events(param, current_genome, int(weight), current_insertion_gene)
+                    Simulation.apply_random_events(param, current_genome, int(weight), current_insertion_gene,
+                                                   current_copy_number)
 
                 # update count of events:
                 ev_node.rearrangements = ev_node.parent_node.rearrangements + rearrangements
                 ev_node.deletions = ev_node.parent_node.deletions + deletions
                 ev_node.insertions = ev_node.parent_node.insertions + insertions
+                ev_node.duplications = ev_node.parent_node.duplications + duplications
 
-
+    # Custom simulation on the model of COSER paper, with D DCJs,
     def open_tree(self, treefile):
         self.sim_tree = Tree.get_from_path(treefile, schema="newick")  # , as_rooted=True)
         self.sim_tree.reroot_at_midpoint()
 
-
-    def simulate_tree(self):
-        tree = treesim.birth_death_tree(birth_rate=0.001, death_rate=0, ntax=param.sim)
+    def simulate_tree(self, ntax):
+        tree = treesim.birth_death_tree(birth_rate=0.001, death_rate=0, ntax=ntax)
         tree.seed_node.edge.length = 0
-        if param.disturb > 0:
-            d = param.disturb
-            for edge in tree.postorder_edge_iter():
-                r = random.random() * 2 * d - d
-                edge.length *= math.exp(r)
+        # disturb and scaling:
+        try:
+            if param.disturb > 0:
+                d = param.disturb
+                for edge in tree.postorder_edge_iter():
+                    r = random.random() * 2 * d - d
+                    edge.length *= math.exp(r)
 
-        # Scaling
-        if self.sim_parameters.rate != 1:
-            tree.scale_edges(param.rate)
-            for edge in tree.postorder_edge_iter():
-                edge.length = round(edge.length, 0) if edge.length is not None else 0
-        elif self.sim_parameters.scale is not None:
-            diameter = algorithms.tree_diameter(tree)
-            tree.scale_edges(self.sim_parameters.scale * self.sim_parameters.num_genes / diameter)
+            # Scaling
+            if self.sim_parameters.rate != 1:
+                tree.scale_edges(param.rate)
+                for edge in tree.postorder_edge_iter():
+                    edge.length = round(edge.length, 0) if edge.length is not None else 0
+            elif self.sim_parameters.scale is not None:
+                diameter = algorithms.tree_diameter(tree)
+                tree.scale_edges(self.sim_parameters.scale * self.sim_parameters.num_genes / diameter)
 
-            # round to integer
-            for edge in tree.postorder_edge_iter():
-                edge.length = round(edge.length, 0) if edge.length is not None else 0
+                # round to integer
+                for edge in tree.postorder_edge_iter():
+                    edge.length = round(edge.length, 0) if edge.length is not None else 0
+        except NameError:
+            pass
         self.sim_tree = tree
 
-    def save_simulation(self):
+    def save_simulation(self, save_copies=False):
         # Output sim result:
         output = self.folder
         if not os.path.exists(output):
@@ -288,20 +314,26 @@ class Simulation:
         self.leaf_genomes = {node.taxon.label: node.value for node in tree.leaf_nodes()}
         self.ancestral_genomes = {node.label: node.value for node in tree.internal_nodes()}
 
-
         # Genomes:
         file_ops.write_genomes_to_file(self.leaf_genomes, os.path.join(output, cfg.sim_leaf_genomes()))
         file_ops.write_genomes_to_file(self.ancestral_genomes, os.path.join(output, cfg.sim_ancestral_genomes()))
 
+        # Copy number, if duplications present:
+        if save_copies:
+            leaf_cn = cfg.sim_leaf_genomes() + cfg.copy_number_file_extension()
+            file_ops.write_genomes_copy_number_to_file(self.leaf_genomes, os.path.join(output, leaf_cn))
+            ancestral_cn = cfg.sim_ancestral_genomes() + cfg.copy_number_file_extension()
+            file_ops.write_genomes_to_file(self.ancestral_genomes, os.path.join(output, ancestral_cn))
+
         # Software-specific files:
         # MLGO, and maybe others, doesn't like the "#" lines; output a 'simple' version:
-        file_ops.write_genomes_to_file(self.leaf_genomes, os.path.join(output, cfg.sim_leaf_simple_genomes()), write_chr_line=False)
+        file_ops.write_genomes_to_file(self.leaf_genomes, os.path.join(output, cfg.sim_leaf_simple_genomes()),
+                                       write_chr_line=False)
 
         # MGRA2:
         file_ops.write_mgra2_config(self.leaf_genomes, tree, os.path.join(output, cfg.sim_mgra_config()))
 
-
-        #TODO: If no indel, also write output for Procar, Pathgroups and GASTS. Other softs?
+        # TODO: If no indel, also write output for Procar, Pathgroups and GASTS. Other softs?
         # # == Procar format:
         #
         # # Genomes:
@@ -325,7 +357,7 @@ class Simulation:
             # f.write("Input Tree\t%s\n" % (param.file if param.file is not None else "BD tree"))
             f.write("Evol.Rate\t%d\n" % param.rate)
             f.write("Num_events\t%d\n" % int(tree.length()))
-            f.write("Avg events per branch\t%.2f\n" % (tree.length() / (len( list(tree.postorder_edge_iter()))-1) ))
+            f.write("Avg events per branch\t%.2f\n" % (tree.length() / (len(list(tree.postorder_edge_iter())) - 1)))
             d = {node.taxon.label: node.distance_from_root() for node in tree.leaf_nodes()}
             f.write("Diameter: %.1f\n" % algorithms.tree_diameter(tree))
             f.write(",".join(["(%s:%d)" % (node, distance) for node, distance in d.iteritems()]))
@@ -336,25 +368,28 @@ class Simulation:
         file_ops.write_simulation_parameters(param, output)
 
 
-
 ## Main: Generate simulation
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-            description="Simulates rearrangement evolution on a given newick tree")
+        description="Simulates rearrangement evolution on a given newick tree")
     parser.add_argument("-n", "--num_genes", type=int, default=100, help="Number of genes in the root genome.")
     parser.add_argument("-c", "--num_chr", type=int, default=5, help="Number of chromosomes in the root genome.")
     parser.add_argument("-o", "--output", type=str, default="sim", help="Name of the output folder.")
     parser.add_argument("-dp", "--deletion_p", type=float, default=0.0, help="Percentage of deletions, from 0 to 1.0")
     parser.add_argument("-ip", "--insertion_p", type=float, default=0.0, help="Percentage of insertions, from 0 to 1.0")
-    parser.add_argument("-dup_p", "--duplication_p", type=float, default=0.0, help="Percentage of duplications, from 0 to 1.0")
-    parser.add_argument("-dl",  "--duplication_length", type=int, default=5, help="Maximum length of duplication event.")
-    parser.add_argument("--indel_length", type=int, default=5, help="Maximum size of indel event in genes.")
+    parser.add_argument("-dup_p", "--duplication_p", type=float, default=0.0,
+                        help="Percentage of duplications, from 0 to 1.0")
+    parser.add_argument("-dl", "--duplication_length", type=int, default=5, help="Maximum length of duplication event.")
+    parser.add_argument("-il", "--indel_length", type=int, default=5, help="Maximum size of indel event in genes.")
     scaling = parser.add_mutually_exclusive_group(required=False)
     scaling.add_argument("-r", "--rate", type=float, default=1, help="Multiplier on the input tree number of events.")
     scaling.add_argument("-sc", "--scale", type=float,
                          help="Scales the tree so each leaf has on average scale/2 *n_genes number of events. Almost the same as diameter, if tree is ultrametric. ")
     # TODO: parameter to overwrite stuff; if not present, do not rewrite on same folder
+    parser.add_argument("--predup", type=int, default=0,
+                        help="Apply a number of duplications in the initial genome, before the random walk.")
+
     tree_input = parser.add_mutually_exclusive_group(required=True)
     tree_input.add_argument("-f", "--file", help="Input a Newick tree")
     tree_input.add_argument("-s", "--sim", type=int, help="Simulate a new birth_death with SIM species")
@@ -367,7 +402,7 @@ if __name__ == '__main__':
     sim_par = SimParameters(num_genes=param.num_genes, num_chr=param.num_chr,
                             del_p=param.deletion_p, ins_p=param.insertion_p, indel_length=param.indel_length,
                             duplication_p=param.duplication_p, duplication_length=param.duplication_length,
-                            rate=param.rate, scale=param.scale, disturb=param.disturb)
+                            rate=param.rate, scale=param.scale, disturb=param.disturb, pre_dup=param.predup)
     # start sim object;
     sim = Simulation(param.output, sim_par)
 
@@ -375,8 +410,8 @@ if __name__ == '__main__':
     if param.file is not None:
         sim.open_tree(param.file)
     else:
-        sim.simulate_tree()
+        sim.simulate_tree(param.sim)
 
     sim.run_simulation()
 
-    sim.save_simulation()
+    sim.save_simulation(save_copies=sim_par.pre_duplications > 0 or sim_par.duplication_p > 0)
